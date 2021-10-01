d8371cd9f2c6c8085bb10db344b31a5c2902b4aa
BorjaEst <boressan@outlook.com>
2021-10-01 11:21:51 +0200

    Fix example routes

--- example/routes.py
+++ example/routes.py
@@ -6,16 +6,16 @@ user_blp = Blueprint('users', __name__)
 admin_blp = Blueprint('admins', __name__)
 
 
-@public_blp.route('/', methods=['GET'])
+@public_blp.route('', methods=['GET'])
 def public_endpoint():
     return Response('', status=204, mimetype='application/json')
 
 
-@user_blp.route('/', methods=['GET'])
+@user_blp.route('', methods=['GET'])
 def users_endpoint():
     return Response('', status=204, mimetype='application/json')
 
 
-@admin_blp.route('/', methods=['GET'])
+@admin_blp.route('', methods=['GET'])
 def admins_endpoint():
     return Response('', status=204, mimetype='application/json')
