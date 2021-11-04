8c030b78a3c9a7a1130f945b7f81622cce187028
BorjaEst <boressan@outlook.com>
2021-11-04 09:52:32 +0100

    Fix function replacement by decorator

--- example/application/authorization.py
+++ example/application/authorization.py
@@ -1,6 +1,6 @@
 import flask_flaat
-from flask import current_app
-from flask_flaat import login_required
+from flask import current_app, abort
+from flask_flaat import login_required, current_userinfo
 
 from .extensions import login_manager
 from .models import User
@@ -9,14 +9,23 @@ __all__ = ['login_required', 'admin_required']
 
 
 @login_manager.user_loader
-def load_user(user_info):
+def load_user(current_userinfo):
     return User.query.get((
-        user_info['body']['sub'],
-        user_info['body']['iss'],
+        current_userinfo['body']['sub'],
+        current_userinfo['body']['iss'],
     ))
 
 
 @flask_flaat.scope_required('eduperson_entitlement')
-def admin_required(entitlements):
+def is_admin():
     any_of = current_app.config['ADMIN_ENTITLEMENTS']
+    entitlements = current_userinfo['eduperson_entitlement']
     return not set(any_of).isdisjoint(entitlements)
+
+
+def admin_required(route):
+    def decorated_view(*args, **kwargs):
+        if not is_admin():
+            abort(403)
+        return route(*args, **kwargs)
+    return login_required(decorated_view)
