49ac9883683ab8ffd4ce813dee3ea264da04a0c2
BorjaEst <boressan@outlook.com>
2021-11-04 09:52:44 +0100

    Pass tests

--- flask_flaat/utils.py
+++ flask_flaat/utils.py
@@ -5,26 +5,25 @@ General utilities.
 import functools
 
 import flask_login
-from flaat import ensure_is_list
 from flask import abort, current_app, has_request_context, request, session
 from werkzeug.local import LocalProxy
 
 
 def login_user(**kwargs):
     flaat = current_app.login_manager._flaat
-    user_info = flaat._get_all_info_from_request(request)
-    if not user_info:
+    userinfo = flaat._get_all_info_from_request(request)
+    if not userinfo:
         abort(401)
 
-    user = current_app.login_manager._user_callback(user_info)
+    user = current_app.login_manager._user_callback(userinfo)
     result = flask_login.login_user(user, **kwargs) if user else abort(401)
 
     # WARNING Dirty code: Patch '_user_id' with what we really want to store
-    session['_user_id'] = user_info    # Save user information
+    session['_user_id'] = userinfo    # Save user information
     return result
 
 
-request_userinfo = LocalProxy(lambda: _get_userinfo())
+current_userinfo = LocalProxy(lambda: _get_userinfo())
 
 
 def _get_userinfo():
@@ -36,43 +35,14 @@ def _get_userinfo():
             return flaat._get_all_info_from_request(request)
 
 
-def group_required(**flaat_kwargs):
+def scope_required(scope):
     def wrapper(func):
         @functools.wraps(func)
         def decorated_view(*args, **kwargs):
-            flaat = current_app.login_manager._flaat
-            _group_required(flaat, request_userinfo, **flaat_kwargs)
+            if current_userinfo is None:
+                abort(401, f"Invalid or missing authentication token")
+            if scope not in current_userinfo:
+                abort(403, f"No scope '{scope}' available at user info")
             return func(*args, **kwargs)
-        return flask_login.login_required(decorated_view)
+        return decorated_view
     return wrapper
-
-
-def _group_required(flaat, user_info, group=[], claim=None, match='all'):
-    if user_info is None:
-        abort(401)
-
-    group = ensure_is_list(group)
-    required_matches = flaat._determine_number_of_required_matches(
-        match, group)
-    if not required_matches:
-        raise RuntimeError(
-            f"Error interpreting the 'match' parameter: {match}")
-
-    # copy entries from incoming claim
-    (avail_group_entries, user_message) = flaat._get_entitlements_from_claim(
-        user_info, claim)
-
-    if not avail_group_entries:
-        abort(403)
-
-    # now we do the actual checking
-    matches_found = 0
-    for entry in avail_group_entries:
-        for g in group:
-            if entry == g:
-                matches_found += 1
-
-    if matches_found < required_matches:
-        abort(403)
-
-    return True
